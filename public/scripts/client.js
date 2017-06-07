7f5f18d8cbb3f2766b125867facc9ff83003dc47
mohamedtwice <mohamedtwice@gmail.com>
2017-06-07 15:00:33 -0500

    almost

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   console.log('JQ');
   customerList();
+  customerOrderInfo();
 }); // end doc ready
 
 
@@ -23,3 +24,24 @@ var customerList = function() {
     } // end success
   }); // end ajax
 }; // end customerList
+
+var customerOrderInfo = function() {
+  $.ajax({
+    type: 'GET',
+    url: 'get/orderinfo',
+    success: function(response) {
+      console.log('back from route with:', response);
+
+      for (var i = 0; i < response.length; i++) {
+        var orderInfo = '<li id="list-li" data-id="' + response[i].id + '">';
+        orderInfo += '<span class="li-text">';
+        orderInfo += response[i].first_name + ' ' + response[i].last_name;
+        orderInfo += '</span>';
+        orderInfo += '<button id="showorders" data-id="' + response[i].id + '" class="btn btn-default btn-xs">Show Orders</button>';
+        $('#clientList').append(listofCustomers);
+
+        $('#clientList').append('<li data-id="' + response[i].id + '">' + response[i].first_name + ' ' + response[i].last_name + '</li>');
+      }
+    } // end success
+  }); // end ajax
+}; // end customerInfo
--- routes/customers.js
+++ routes/customers.js
@@ -17,12 +17,6 @@ var sendBackData = [];
 
 router.get('/customers', function(req, res) {
   pool.connect(function(err, client, done) {
-    // if (err) {
-    //   console.log('Error connecting to the DB', err);
-    //   res.sendStatus(500);
-    //   done();
-    //   return;
-    // }
 
     client.query('SELECT * FROM customers;', function(err, result) {
       done();
@@ -37,5 +31,24 @@ router.get('/customers', function(req, res) {
     });
   });
 });
+
+router.get('/orderinfo', function(req, res) {
+  pool.connect(function(err, client, done) {
+    console.log("hitting /orderinfo route");
+    client.query('SELECT (customer_id), (first_name, last_name), (order_id), (street), (city), (zip), (description), (products.unit_price), (quantity)  FROM customers JOIN addresses ON customers.id = addresses.customer_id JOIN orders ON orders.address_id = addresses.id JOIN line_items ON line_items.order_id = orders.id JOIN products ON products.id = line_items.product_id;', function(err, result) {
+      done();
+      if (err) {
+        console.log('Error querying the DB', err);
+        res.sendStatus(500);
+        return;
+      }
+
+      console.log('Got rows from the DB:', result.rows);
+      res.send(result.rows);
+    });
+  });
+});
+
+
 
 module.exports = router;
